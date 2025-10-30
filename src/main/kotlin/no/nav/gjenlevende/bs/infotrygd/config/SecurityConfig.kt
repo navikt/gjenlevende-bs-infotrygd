05d4d501de4269da638235d9581a99820a3caf1c
olekvernberg <ole.christian.kvernberg@nav.no>
2025-10-30 15:14:45 +0100

    Config actuator og tillat kall til isAlive

---
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/config/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package no.nav.gjenlevende.bs.infotrygd.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+open class SecurityConfig {
+
+    @Bean
+    open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/internal/**", "/actuator/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .csrf { it.disable() }
+
+        return http.build()
+    }
+}
