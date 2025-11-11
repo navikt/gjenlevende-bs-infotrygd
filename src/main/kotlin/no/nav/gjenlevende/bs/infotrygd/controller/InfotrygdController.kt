88e1786ededc4f17b7f0ef89583bb14bb2050381
krikof-nav <kristian.kofoed@nav.no>
2025-11-11 12:08:38 +0100

    Legger til Jwt håndtering fra gjenlevende-bs-sak.

--- src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/controller/InfotrygdController.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/controller/InfotrygdController.kt
@@ -1,16 +1,29 @@
 package no.nav.gjenlevende.bs.infotrygd.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/infotrygd")
 class InfotrygdController {
 
     @GetMapping("/ping")
-    fun ping(): ResponseEntity<String> = ResponseEntity.ok("Pong fra gjenlevende-bs-infotrygd")
+    @PreAuthorize("hasRole('SAKSBEHANDLER')")
+    fun ping(
+        @AuthenticationPrincipal jwt: Jwt,
+    ): ResponseEntity<Map<String, String>> {
+        val navIdent = jwt.getClaimAsString("NAVident")
+
+        return ResponseEntity.ok(
+            mapOf(
+                "melding" to "Pong fra gjenlevende-bs-infotrygd",
+                "navIdent" to navIdent,
+            ),
+        )
+    }
 }
