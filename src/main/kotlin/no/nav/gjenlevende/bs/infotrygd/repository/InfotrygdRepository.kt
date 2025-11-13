0fad1c8caf4faaf044d673ec1ce7ed7efe3f7d8f
olekvernberg <ole.christian.kvernberg@nav.no>
2025-11-13 13:06:11 +0100

    * Lagt til jdbc oppsett og repo fil * Merge from main

---
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/repository/InfotrygdRepository.kt
@@ -0,0 +1,44 @@
+package no.nav.gjenlevende.bs.infotrygd.repository
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Repository
+import java.time.LocalDate
+
+@Repository
+class InfotrygdRepository(
+    private val jdbcTemplate: NamedParameterJdbcTemplate,
+) {
+
+    fun harStønad(
+        personIdenter: Set<String>,
+        kunAktive: Boolean = false,
+        dagensDato: LocalDate = LocalDate.now(),
+    ): List<String> {
+        val values =
+            MapSqlParameterSource()
+                .addValue("personIdenter", personIdenter)
+        val filter: String =
+            if (kunAktive) {
+                values.addValue("dagensDato", dagensDato)
+                " AND nvl(S.DATO_OPPHOR,V.DATO_INNV_TOM) > :dagensDato "
+            } else {
+                ""
+            }
+
+        val result =
+            jdbcTemplate.query(
+        """
+                SELECT L.PERSONNR
+                  FROM T_BESLUT B
+                WHERE L.PERSONNR IN (:personIdenter)
+                  $filter
+                GROUP BY L.personnr
+            """,
+                values,
+            ) { resultSet, _ ->
+                resultSet.getString("PERSONNR")
+            }
+        return result.toList()
+    }
+}
