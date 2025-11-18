b23de0c1c8500c27fd5c20d904c07c615a38df1b
olekvernberg <ole.christian.kvernberg@nav.no>
2025-11-18 10:45:54 +0100

    Test sett schema i query

--- src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/repository/InfotrygdRepository.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/repository/InfotrygdRepository.kt
@@ -45,7 +45,7 @@ class InfotrygdRepository(
         val result =
             jdbcTemplate.query(
                 """
-                select TEKST from T_GRADSTYPE
+                select TEKST from EB_INFOTRYGD_Q1.T_GRADSTYPE
                 """,
             ) { resultSet, _ ->
                 resultSet.getString("TEKST")
