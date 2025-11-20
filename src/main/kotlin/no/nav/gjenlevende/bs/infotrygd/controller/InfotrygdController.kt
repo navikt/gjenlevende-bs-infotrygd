ba4d90f2115c1da9d0f37c647b69632b0bac7a1a
krikof-nav <kristian.kofoed@nav.no>
2025-11-20 10:44:08 +0100

    Legger til logging og bedre response body for feil ved oppslag av perioder.

--- src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/controller/InfotrygdController.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/controller/InfotrygdController.kt
@@ -3,6 +3,7 @@ package no.nav.gjenlevende.bs.infotrygd.controller
 import no.nav.gjenlevende.bs.infotrygd.dto.VedtakPeriodeRequest
 import no.nav.gjenlevende.bs.infotrygd.dto.VedtakPeriodeResponse
 import no.nav.gjenlevende.bs.infotrygd.service.InfotrygdService
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,12 +16,22 @@ import org.springframework.web.bind.annotation.RestController
 open class InfotrygdController(
     private val infotrygdService: InfotrygdService,
 ) {
+    private val logger = LoggerFactory.getLogger(InfotrygdController::class.java)
+
     @PostMapping("/perioder")
     @PreAuthorize("hasRole('SAKSBEHANDLER') and hasRole('BESLUTTER') and hasRole('VEILEDER')")
     fun hentPerioderForPerson(
         @RequestBody request: VedtakPeriodeRequest,
     ): ResponseEntity<VedtakPeriodeResponse> {
-        val perioder = infotrygdService.hentVedtakPerioder(request.personident)
-        return ResponseEntity.ok(perioder)
+        logger.info("Mottatt forespørsel om å hente perioder for person")
+
+        return try {
+            val perioder = infotrygdService.hentVedtakPerioder(request.personident)
+            logger.info("Returnerer perioder: ${perioder.barnetilsyn.size} barnetilsyn, ${perioder.skolepenger.size} skolepenger")
+            ResponseEntity.ok(perioder)
+        } catch (e: Exception) {
+            logger.error("Feil ved henting av perioder fra Infotrygd: ${e.message}", e)
+            throw e
+        }
     }
 }
