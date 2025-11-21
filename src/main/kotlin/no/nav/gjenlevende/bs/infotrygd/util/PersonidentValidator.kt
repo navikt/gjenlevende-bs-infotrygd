53317e8c76f0addfa632acd62c993eb568b71c06
krikof-nav <kristian.kofoed@nav.no>
2025-11-21 10:23:18 +0100

    Refaktorering.

--- src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/util/PersonidentValidator.kt
+++ src/main/kotlin/no/nav/gjenlevende/bs/infotrygd/util/PersonidentValidator.kt
@@ -4,11 +4,8 @@ object PersonidentValidator {
     private val PERSONIDENT_REGEX = """[0-9]{11}""".toRegex()
 
     fun validerPersonident(personident: String) {
-        if (personident.length != 11) {
-            throw IllegalArgumentException("Ugyldig personident. Det må være 11 siffer, fikk ${personident.length} tegn")
-        }
-        if (!PERSONIDENT_REGEX.matches(personident)) {
-            throw IllegalArgumentException("Ugyldig personident. Det kan kun inneholde tall")
+        if (personident.length != 11 || !PERSONIDENT_REGEX.matches(personident)) {
+            throw IllegalArgumentException("Ugyldig personident.")
         }
     }
 
